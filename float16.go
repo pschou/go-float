cd1ce0bde47d67aa1ecaab73f8a04748cd7e8de1
schou <pschou@users.noreply.github.com>
2024-12-20 10:22:10 -0500

    shifting a bit

--- float16.go
+++ float16.go
@@ -46,7 +46,7 @@ func Squeeze16(f float32, expBits, expShift int) uint16 {
 	in := math.Float32bits(f)
 	sign := in & 0x80000000
 	max := 1 << (expBits - 1)
-	exp := int(in&0x7ff80000)>>23 - 126 - expShift
+	exp := int(in&0x7ff80000)>>23 - 127 - expShift
 	if exp > max {
 		return 0x7fff | uint16(sign>>16)
 	} else if exp < 0 {
@@ -72,7 +72,7 @@ func Squeeze16(f float32, expBits, expShift int) uint16 {
 func SqueezeU16(f float32, expBits, expShift int) uint16 {
 	in := math.Float32bits(f)
 	max := 1 << (expBits - 1)
-	exp := int(in&0x7ff80000)>>23 - 126 - expShift
+	exp := int(in&0x7ff80000)>>23 - 127 - expShift
 	if exp > max {
 		return 0xffff
 	} else if exp < 0 {
@@ -87,13 +87,19 @@ func SqueezeU16(f float32, expBits, expShift int) uint16 {
 // original, the represented value.
 func Expand16(in uint16, expBits, expShift int) float32 {
 	sign := uint32(in&0x8000) << 16
-	exp := (int(0x7fff&in) >> (15 - expBits)) + 126 + expShift
+	exp := (int(0x7fff&in) >> (15 - expBits)) + 127 + expShift
+	if exp > 189 {
+		exp, in = 189, 0xffff
+	}
 	return math.Float32frombits(uint32(in<<(expBits+1))<<7 | sign | uint32(exp<<23))
 }
 
 // Use this to expand the [SqueezeU16] and restore the number back to like the
 // original, the represented value.
 func ExpandU16(in uint16, expBits, expShift int) float32 {
-	exp := (int(in) >> (16 - expBits)) + 126 + expShift
-	return math.Float32frombits(uint32(in<<(expBits))<<7 | uint32(exp<<23))
+	exp := int(in>>(16-expBits)) + 127 + expShift
+	if exp > 189 {
+		exp, in = 189, 0xffff
+	}
+	return math.Float32frombits(uint32(in<<(expBits))<<7 | uint32(exp)<<23)
 }
--- float16_test.go
+++ float16_test.go
@@ -16,30 +16,30 @@ func ExampleSqueezeU16() {
 
 func ExampleSqueeze16() {
 	// Fits within 1 to 3.999
-	b := float.Squeeze16(3.1415, 1, 1)
-	exp := float.Expand16(b, 1, 1)
-	fmt.Println("1,1 float", exp)
-
-	// Does not fit within 0.5 to 1.999
-	b = float.Squeeze16(3.1415, 1, 0)
-	exp = float.Expand16(b, 1, 0)
+	b := float.Squeeze16(3.1415, 1, 0)
+	exp := float.Expand16(b, 1, 0)
 	fmt.Println("1,0 float", exp)
 
+	// Does not fit within 0.5 to 1.999
+	b = float.Squeeze16(3.1415, 1, -1)
+	exp = float.Expand16(b, 1, -1)
+	fmt.Println("1,-1 float", exp)
+
 	// Fits within 2 to 7.999
+	b = float.Squeeze16(3.1415, 1, 1)
+	exp = float.Expand16(b, 1, 1)
+	fmt.Println("1,1 float", exp)
+
+	// Does not fit within 4 to 15.999
 	b = float.Squeeze16(3.1415, 1, 2)
 	exp = float.Expand16(b, 1, 2)
 	fmt.Println("1,2 float", exp)
 
-	// Does not fit within 4 to 15.999
-	b = float.Squeeze16(3.1415, 1, 3)
-	exp = float.Expand16(b, 1, 3)
-	fmt.Println("1,3 float", exp)
-
 	// Output:
+	// 1,0 float 3.1414795
+	// 1,-1 float 1.999939
 	// 1,1 float 3.1414795
-	// 1,0 float 1.999939
-	// 1,2 float 3.1414795
-	// 1,3 float 4
+	// 1,2 float 4
 }
 
 func ExampleSpreadU16() {
@@ -55,15 +55,23 @@ func ExampleSpreadU16() {
 	a, z = float.SpreadU16(1, 1)
 	fmt.Println("1,1 lower", a, "upper", z)
 
-	a, z = float.SpreadU16(3, 1)
-	fmt.Println("0,2 lower", a, "upper", z)
+	a, z = float.SpreadU16(4, 1)
+	fmt.Println("4,1 lower", a, "upper", int(z))
+
+	a, z = float.SpreadU16(5, 1)
+	fmt.Println("5,1 lower", a, "upper", int(z))
+
+	a, z = float.SpreadU16(6, 1)
+	fmt.Println("6,1 lower", a, "upper", int(z))
 
 	// Output:
-	// 0,0 lower 0.5 upper 0.9999924
-	// 0,1 lower 1 upper 1.9999847
-	// 1,0 lower 0.5 upper 1.9999695
-	// 1,1 lower 1 upper 3.999939
-	// 0,2 lower 1 upper 255.98438
+	// 0,0 lower 1 upper 1.9999847
+	// 0,1 lower 2 upper 3.9999695
+	// 1,0 lower 1 upper 3.999939
+	// 1,1 lower 2 upper 7.999878
+	// 4,1 lower 2 upper 131056
+	// 5,1 lower 2 upper 8587837440
+	// 6,1 lower 2 upper 9218868437227405312
 }
 func ExampleSpread16() {
 	a, z := float.Spread16(0, 0)
@@ -79,12 +87,12 @@ func ExampleSpread16() {
 	fmt.Println("1,1 lower", a, "upper", z)
 
 	a, z = float.Spread16(3, 1)
-	fmt.Println("0,2 lower", a, "upper", z)
+	fmt.Println("3,1 lower", a, "upper", z)
 
 	// Output:
-	// 0,0 lower 0.5 upper 0.99998474
-	// 0,1 lower 1 upper 1.9999695
-	// 1,0 lower 0.5 upper 1.999939
-	// 1,1 lower 1 upper 3.999878
-	// 0,2 lower 1 upper 255.96875
+	// 0,0 lower 1 upper 1.9999695
+	// 0,1 lower 2 upper 3.999939
+	// 1,0 lower 1 upper 3.999878
+	// 1,1 lower 2 upper 7.999756
+	// 3,1 lower 2 upper 511.9375
 }
